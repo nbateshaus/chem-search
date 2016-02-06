024ba93e0633f9e0a438a0429d59d6551a5c0bd0
Greg Landrum <greg.landrum@gmail.com>
2016-02-06 05:04:32 +0100

    add a bit of docs

--- mol-renderer/render.py
+++ mol-renderer/render.py
@@ -14,6 +14,7 @@ def health():
 
 @app.route('/canon_smiles/<smiles>')
 def canon_smiles(smiles):
+    " returns canonical SMILES for an input SMILES "
     return Chem.CanonSmiles(smiles)
 
 def _prepareMol(mol,kekulize):
@@ -49,6 +50,7 @@ def _render(mol,renderer,size=(150,100),**kwargs):
 
 @app.route('/mol_to_png/mol.png', methods=['GET', 'POST'])
 def mol_to_png():
+    " returns a PNG for a mol block "
     molblock = request.values.get('mol')
     m = Chem.MolFromMolBlock(molblock)
     response = make_response(_render(m,_moltopng))
@@ -56,6 +58,7 @@ def mol_to_png():
     return response
 @app.route('/mol_to_svg/mol.svg', methods=['GET', 'POST'])
 def mol_to_svg():
+    " returns an SVG for a mol block "
     molblock = request.values.get('mol')
     m = Chem.MolFromMolBlock(molblock)
     response = make_response(_render(m,_moltosvg))
@@ -63,6 +66,7 @@ def mol_to_svg():
 
 @app.route('/smiles_to_png/<smiles>.png', methods=['GET', 'POST'])
 def smiles_to_png(smiles):
+    " returns a PNG for a SMILES "
     m = Chem.MolFromSmiles(smiles)
     response = make_response(_render(m,_moltopng))
     response.headers['Content-Type'] = 'image/png'
@@ -70,6 +74,7 @@ def smiles_to_png(smiles):
 
 @app.route('/smiles_to_svg/<smiles>.svg', methods=['GET', 'POST'])
 def smiles_to_svg(smiles):
+    " returns an SVG for a SMILES "
     m = Chem.MolFromSmiles(smiles)
     response = make_response(_render(m,_moltosvg))
     #response.headers['Content-Type'] = 'image/svg+xml' # SVG doesn't render properly in chrome if we send this
