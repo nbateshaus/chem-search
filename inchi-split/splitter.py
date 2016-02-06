9d1017c94f242564224d49da4837b5aa700c4f46
Greg Landrum <greg.landrum@gmail.com>
2016-02-06 10:51:50 +0100

    a bit of the fixedh stereo. needs more tests

--- inchi-split/splitter.py
+++ inchi-split/splitter.py
@@ -14,10 +14,16 @@ r"(/b[\-\+0-9,\?]*)?", # stereo_bond
 r"(/t[\-\+0-9,\?]*)?", #stereo_tet  FIX: probably could be tightened up
 r"(/m[\-\+0-9,]*)?", #stereo_m    FIX: probably could be tightened up
 r"(/s[\-\+0-9,]*)?", #stereo_s    FIX: probably could be tightened up
+r"(/f/h[0-9,\-\H\(\)]*)?", # fixed_h
+r"(/b[\-\+0-9,\?]*)?", #fixed_h_stereo_bond
+r"(/t[\-\+0-9,\?]*)?", #fixedh_stereo_tet  FIX: probably could be tightened up
+r"(/m[\-\+0-9,]*)?", #fixedh_stereo_m    FIX: probably could be tightened up
+r"(/s[\-\+0-9,]*)?", #fixedh_stereo_s    FIX: probably could be tightened up
 
 )
 coreExpr=re.compile(''.join(inchiLayers))
-Layers=namedtuple("Layers",['start','formula','skeleton','hydrogens','charge','protonation','stereo_bond','stereo_tet','stereo_m','stereo_s'])#,'fixedh','fixedh_stereo'])
+Layers=namedtuple("Layers",['start','formula','skeleton','hydrogens','charge','protonation','stereo_bond','stereo_tet','stereo_m','stereo_s',
+                            'fixedh','fixedh_stereo_bond','fixedh_stereo_tet','fixedh_stereo_m','fixedh_stereo_s'])
 def extractLayers(inchi):
     """
 
@@ -42,16 +48,20 @@ def extractLayers(inchi):
     'm0'
     >>> tpl.stereo_s
     's1'
+    >>> tpl.fixedh
+    ''
 
     Charge layers:
-    >>> tpl = extractLayers('InChI=1S/C4H10NO/c5-3-1-2-4-6/h1-5H2/q-1/p+1') # from [O-]CCCC[NH3+]
+    # from [O-]CCCC[NH3+]
+    >>> tpl = extractLayers('InChI=1S/C4H10NO/c5-3-1-2-4-6/h1-5H2/q-1/p+1')
     >>> tpl.charge
     'q-1'
     >>> tpl.protonation
     'p+1'
 
     Stereochemistry:
-    >>> tpl = extractLayers('InChI=1S/C9H12ClN2O3/c10-7(13)3-1-2-6(4-8(11)14)5-9(12)15/h1-3,7H,4-5H2,(H2,11,14)(H2,12,15)/q-1/b3-1+/t7-/m0/s1') # from [O-][C@H](Cl)/C=C/C=C(/CC(O)=N)CC(=O)N
+    # from [O-][C@H](Cl)/C=C/C=C(/CC(O)=N)CC(=O)N
+    >>> tpl = extractLayers('InChI=1S/C9H12ClN2O3/c10-7(13)3-1-2-6(4-8(11)14)5-9(12)15/h1-3,7H,4-5H2,(H2,11,14)(H2,12,15)/q-1/b3-1+/t7-/m0/s1')
     >>> tpl.stereo_bond
     'b3-1+'
     >>> tpl.stereo_tet
@@ -60,6 +70,23 @@ def extractLayers(inchi):
     'm0'
     >>> tpl.stereo_s
     's1'
+
+    Fixed Hs:
+    # From: F[C@H](Cl)/C=C/C=C(/CC(O)=N)CC(=O)N
+    >>> tpl = extractLayers('InChI=1/C9H12ClFN2O2/c10-7(11)3-1-2-6(4-8(12)14)5-9(13)15/h1-3,7H,4-5H2,(H2,12,14)(H2,13,15)/b3-1+/t7-/m0/s1/f/h12,14H,13H2/b3-1+,6-2-,12-8?')
+    >>> tpl.fixedh
+    'f/h12,14H,13H2'
+    >>> tpl.fixedh_stereo_bond
+    'b3-1+,6-2-,12-8?'
+
+    # From: C[C@H](Cl)[C@H](/CC(O)=N)CC(=O)N
+    >>> tpl = extractLayers('InChI=1/C7H13ClN2O2/c1-4(8)5(2-6(9)11)3-7(10)12/h4-5H,2-3H2,1H3,(H2,9,11)(H2,10,12)/t4-/m0/s1/f/h9,11H,10H2/t4-,5+')
+    >>> tpl.fixedh
+    'f/h9,11H,10H2'
+    >>> tpl.fixedh_stereo_tet
+    't4-,5+'
+
+
 
     Edge cases:
     >>> tpl=extractLayers('InChI=1S/H2/h1H')
