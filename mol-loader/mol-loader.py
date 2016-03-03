751faff303ad7ac3e35505c199a1041afe9310a5
Nik Bates-Haus <nik.bates-haus@tamr.com>
2016-03-02 21:53:51 -0500

    Wire in Surechembl

--- mol-loader/mol-loader.py
+++ mol-loader/mol-loader.py
@@ -1,6 +1,7 @@
 import Chembl
 import Pubchem
 import Solr
+import Surechembl
 
 if __name__=='__main__':
     s = Solr.Solr()
@@ -8,3 +9,5 @@ if __name__=='__main__':
     s.postall(chembl)
     pubchem = Pubchem.Pubchem(path="/Users/nik/Data/PubChem/*.sdf.gz")
     s.postall(pubchem)
+    surechembl = Surechembl.Surechembl(path="/Users/nik/Data/PubChem/*.sdf.gz")
+    s.postall(surechembl)
