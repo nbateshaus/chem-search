abaf0f07fc6455ac35a847375d8014d85711e9b2
Nik Bates-Haus <nik.bates-haus@tamr.com>
2016-03-17 19:08:03 -0400

    Results hooked up to Solr; pagination works.

--- SearchApp/index.py
+++ SearchApp/index.py
@@ -1,12 +1,32 @@
-from flask import render_template
+from flask import render_template, request, url_for
 
 from SearchApp import app
 
+import pysolr
+
+PAGE_SIZE = 10
 
 @app.route('/')
 @app.route('/index')
 def index():
+    args = request.args.to_dict()
+    page=1
+    if 'page' in args:
+        page=int(args['page'])
+    start=int((page - 1) * PAGE_SIZE)
+    solr = pysolr.Solr('http://localhost:8983/solr/molecules/')
+    results = solr.search(q="*:*", start=start, rows=PAGE_SIZE)
+    headers = list({header for doc in results.docs for header in doc.keys()})
+    headers.sort()
+    rows = [[doc[header] if header in doc else None for header in headers] for doc in results.docs]
+    pagination = lambda: None
+    pagination.page = page
+    pagination.pages = int(results.hits / PAGE_SIZE)
+
     return render_template(
         'index.html',
-        title='ChemSearch'
+        title='ChemSearch',
+        headers=headers,
+        rows=rows,
+        pagination=pagination
     )
