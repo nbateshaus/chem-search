4aba1e055962a741afa37c2b41be83f345e0af18
Nik Bates-Haus <nik.bates-haus@tamr.com>
2016-03-16 22:10:15 -0400

    First slice of a Flask / Boostrap app for ChemSearch. Just has a nav bar; doesn't do anything.

---
+++ SearchApp/index.py
@@ -0,0 +1,12 @@
+from flask import render_template
+
+from SearchApp import app
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template(
+        'index.html',
+        title='ChemSearch'
+    )
