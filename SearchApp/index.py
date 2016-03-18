b032e4f6b81cfa986b508a98b08e61bf2d2a9016
Nik Bates-Haus <nik.bates-haus@tamr.com>
2016-03-17 20:09:26 -0400

    Appease IntelliJ's formatting foibles.

--- SearchApp/index.py
+++ SearchApp/index.py
@@ -5,22 +5,29 @@ from SearchApp import app
 
 PAGE_SIZE = 10
 
+
+class Pagination:
+    def __init__(self, page, pages):
+        self.page = page
+        self.pages = pages
+
+
 @app.route('/')
 @app.route('/index')
 def index():
     args = request.args.to_dict()
-    page=1
+    page = 1
     if 'page' in args:
-        page=int(args['page'])
-    start=int((page - 1) * PAGE_SIZE)
+        page = int(args['page'])
+    start = int((page - 1) * PAGE_SIZE)
     solr = pysolr.Solr('http://localhost:8983/solr/molecules/')
     results = solr.search(q="*:*", start=start, rows=PAGE_SIZE)
-    headers = list({header for doc in results.docs for header in doc.keys() if not header.startswith('_')})
+    headers = list(
+        {header for doc in results.docs for header in doc.keys() if not header.startswith('_')}
+    )
     headers.sort()
     rows = [[doc[header] if header in doc else None for header in headers] for doc in results.docs]
-    pagination = lambda: None
-    pagination.page = page
-    pagination.pages = int(results.hits / PAGE_SIZE)
+    pagination = Pagination(page, int(results.hits / PAGE_SIZE))
 
     return render_template(
         'index.html',
