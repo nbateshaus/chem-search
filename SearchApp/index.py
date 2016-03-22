b3e1ad4ff3529094aacda02af04d8bcb5cd4af33
Nik Bates-Haus <nik.bates-haus@tamr.com>
2016-03-22 09:21:39 -0400

    Info on # of docs and time. Searching with no value set clears search.

--- SearchApp/index.py
+++ SearchApp/index.py
@@ -1,5 +1,5 @@
 import pysolr
-from flask import render_template, request
+from flask import render_template, request, g
 
 from SearchApp import app
 
@@ -7,9 +7,14 @@ PAGE_SIZE = 10
 
 
 class Pagination:
-    def __init__(self, page, pages):
+    def __init__(self, page, pages, page_size):
         self.page = page
         self.pages = pages
+        self.page_size = page_size
+
+
+def include_field(field):
+    return field.startswith('rdkit_') or field == 'id'
 
 
 @app.route('/')
@@ -20,17 +25,18 @@ def index():
     if 'page' in args:
         page = int(args['page'])
     q='*:*'
-    if 'q' in args:
+    if 'q' in args and args['q'] != '':
         q = args['q']
     start = int((page - 1) * PAGE_SIZE)
     solr = pysolr.Solr('http://localhost:8983/solr/molecules/')
     results = solr.search(q=q, start=start, rows=PAGE_SIZE)
     headers = list(
-        {header for doc in results.docs for header in doc.keys() if not header.startswith('_')}
+        {header for doc in results.docs for header in doc.keys() if include_field(header)}
     )
     headers.sort()
-    rows = [[doc[header] if header in doc else None for header in headers] for doc in results.docs]
-    pagination = Pagination(page, int(results.hits / PAGE_SIZE))
+    rows = [[doc[header] if header in doc else '' for header in headers] for doc in results.docs]
+    pagination = Pagination(page, int(results.hits / PAGE_SIZE), PAGE_SIZE)
+    g.qtime = results.qtime
 
     return render_template(
         'index.html',
