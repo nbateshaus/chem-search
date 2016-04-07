ca6c6868c0558fabd5f7d9e8ffcdaf5e8ca72322
Nik Bates-Haus <nik.bates-haus@tamr.com>
2016-04-07 10:24:27 -0400

    Seems to work, but doesn't actually sort. Sort might require an integer.

--- TanimotoSimilarity/src/org/rdkit/TanimotoSimilarityParser.java
+++ TanimotoSimilarity/src/org/rdkit/TanimotoSimilarityParser.java
@@ -15,8 +15,8 @@ import org.apache.solr.search.ValueSourceParser;
 
 /**
  * Function plugin providing 'tanimoto' function.
- *
- * Use this as tanimoto(field,"FingerPrintStringLiteral"). The result is the Tanimoto similarity
+ * <p>
+ * Use this as tanimoto(numBits, field, "BitPrintStringLiteral"). The result is the Tanimoto similarity
  * metric between the value(s) in the field, and the BitVector represented by the string literal.
  */
 public class TanimotoSimilarityParser extends ValueSourceParser {
@@ -28,10 +28,12 @@ public class TanimotoSimilarityParser extends ValueSourceParser {
 
 	private class TanimotoSimilarity extends ValueSource {
 		final String NAME = "tanimoto";
+		private final int numBits;
 		private final ValueSource source;
 		private final String fps;
 
-		TanimotoSimilarity(ValueSource source, String fps) {
+		TanimotoSimilarity(int numBits, ValueSource source, String fps) {
+			this.numBits = numBits;
 			this.source = source;
 			this.fps = fps;
 		}
@@ -39,33 +41,39 @@ public class TanimotoSimilarityParser extends ValueSourceParser {
 		@Override
 		public FunctionValues getValues(Map context, LeafReaderContext readerContext) throws IOException {
 			final FunctionValues values = source.getValues(context, readerContext);
-			final ExplicitBitVect fp1 = new ExplicitBitVect(fps);
+			final ExplicitBitVect fp1 = new ExplicitBitVect(numBits);
+			RDKFuncs.FromBitString(fp1, fps);
 			return new FunctionValues() {
 				@Override
 				public float floatVal(int doc) {
-					return (float)doubleVal(doc);
+					return (float) doubleVal(doc);
 				}
+
 				@Override
 				public int intVal(int doc) {
-					return (int)doubleVal(doc);
+					return (int) doubleVal(doc);
 				}
+
 				@Override
 				public long longVal(int doc) {
-					return (long)doubleVal(doc);
+					return (long) doubleVal(doc);
 				}
+
 				@Override
 				public double doubleVal(int doc) {
 					final String fps2 = values.strVal(doc);
-					final ExplicitBitVect fp2 = new ExplicitBitVect(fps2);
+					final ExplicitBitVect fp2 = new ExplicitBitVect(numBits);
 					return RDKFuncs.TanimotoSimilarityEBV(fp1, fp2);
 				}
+
 				@Override
 				public String strVal(int doc) {
 					return Double.toString(doubleVal(doc));
 				}
+
 				@Override
 				public String toString(int doc) {
-					return NAME + '(' + values.toString(doc) + ',' + fps + ')';
+					return NAME + '(' + numBits + ',' + values.toString(doc) + ",\"" + fps + "\")";
 				}
 			};
 		}
@@ -81,9 +89,9 @@ public class TanimotoSimilarityParser extends ValueSourceParser {
 			if (!(o instanceof TanimotoSimilarity)) {
 				return false;
 			}
-			TanimotoSimilarity other = (TanimotoSimilarity)o;
+			TanimotoSimilarity other = (TanimotoSimilarity) o;
 			return (other.fps == null ? this.fps == null : other.fps.equals(this.fps)) &&
-				(other.source == null? this.source == null : other.source.equals(this.source));
+				(other.source == null ? this.source == null : other.source.equals(this.source));
 		}
 
 		@Override
@@ -100,13 +108,15 @@ public class TanimotoSimilarityParser extends ValueSourceParser {
 
 		@Override
 		public String description() {
-			return NAME + '(' + source.description() + ',' + "string)";
+			return NAME + '(' + numBits + ',' + source.description() + ",\"" + fps + "\")";
 		}
 	}
+
 	@Override
 	public ValueSource parse(FunctionQParser functionQParser) throws SyntaxError {
+		int numBits = functionQParser.parseInt();
 		ValueSource source = functionQParser.parseValueSource();
 		String fps = functionQParser.parseArg();
-		return new TanimotoSimilarity(source, fps);
+		return new TanimotoSimilarity(numBits, source, fps);
 	}
 }
